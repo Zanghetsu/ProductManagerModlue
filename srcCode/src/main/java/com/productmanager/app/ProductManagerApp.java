87ef7b4c724d8d9f3ea08b7c7fe0d029993440de
Zanghetsu <boythasx@gmail.com>
2021-09-16 10:36:21 +0200

    Created aggregator and product classes

---
+++ srcCode/src/main/java/com/productmanager/app/ProductManagerApp.java
@@ -0,0 +1,17 @@
+package com.productmanager.app;
+
+import com.productmanager.data.Product;
+
+import java.math.BigDecimal;
+
+public class ProductManagerApp {
+    public static void main(String[] args) {
+        Product p1 = new Product();
+
+        p1.setId(101);
+        p1.setName("Coca Cola");
+        p1.setPrice(BigDecimal.valueOf(1.99));
+
+        System.out.println(p1.getId() +" "+p1.getPrice());
+    }
+}
